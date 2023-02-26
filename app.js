64ad1b97046e48a8de1275c6da1889d3eac552d8
lokeshkhade@gmail.com <lokeshkhade@gmail.com>
2023-02-26 21:45:12 +0530

    6- Environments

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const Joi = require('joi');//joi module return a Class and By covention class na
 const express = require('express'); //Load express moudule which returns a function express
 const app = express(); //express fucntion retuns object of type express,by convention we call the object as app.app object support varios method get,post,put
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+//In production we set NODE_ENV=production
+console.log(`app: ${app.get('env')}`);
+
 const logger = require('./logger');
 
 //To enable parsing of JSON object in the body of request
@@ -12,8 +16,12 @@ app.use(express.json());
 //http://expressjs.com/en/api.html#express.urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan('dev'));//Not use in production
+if (app.get('env') === 'development') {
 
+    app.use(morgan('dev'));//Not use in production
+    console.log('Morgan Enabled');
+
+}
 /*A middleware function is basically a function that takes a request object and return the response to client or either terminates the request/response cycle or passes control to another middleware function.Ex. Route Handler Function beacuse it take req as object and return the response to client.So it terminate the request response cycle.*/
 //Another ex: express.json() when we call express.json() method this method return a middleware function the job of this middleware function is to read the request and if there is json object in the body of request it will parse the body of request into a json object then it will set it req.body property.
 //express.json passes the json object to route handler function.It is builtin middleware function.
